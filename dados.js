47f5ed4f00f9332bc34cd007919210933122996d
felippeclopes2-maker <felippeclopes2@gmail.com>
2025-12-22 20:27:23 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,7 +1,8 @@
 const coletanea = {
     "COLETANEA": [
         { "num": "2201", "titulo": "O Senhor é o meu pastor" },
-        { "num": "4405", "titulo": "A minha alma anseia" }
+        { "num": "1", "titulo": "Quem é Esse?" }
         // ... adicione apenas a linha com número e título aqui
     ]
+
 };
