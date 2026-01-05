5d7c1d70e0e234046fcf125e47fe1af5460c3026
felippeclopes2-maker <felippeclopes2@gmail.com>
2026-01-05 17:07:29 -0300

    Update title in coletanea object

--- dados.js
+++ dados.js
@@ -1,10 +1,11 @@
 const coletanea = {
     "COLETANEA": [
-        { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus (Isaias Saad)" }
+        { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus (Isaias Saad) - (Te amo Deus, tua graça nunca falha)" }
         // ... adicione apenas a linha com número e título aqui
     ]
 
 };
+
 
 
 
