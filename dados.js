a06b6dfabf0664037c7a83e6c1eb7698d4f1a9e5
felippeclopes2-maker <felippeclopes2@gmail.com>
2026-01-05 15:54:31 -0300

    Fix entry format in coletanea object

--- dados.js
+++ dados.js
@@ -3,11 +3,12 @@ const coletanea = {
         { "num": "2201", "titulo": "O Senhor é o meu pastor" },
         { "num": "1", "titulo": "Quem é Esse?" },
         { "num": "002", "titulo": "Bondade de Deus" },
-        { "titulo": "Bondade de Deus (Isaias Saad)" }
+        { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus (Isaias Saad)" }
         // ... adicione apenas a linha com número e título aqui
     ]
 
 };
+
 
 
 
