9ddd08a990d3174e897584acc701bb3090bc2c10
felippeclopes2-maker <felippeclopes2@gmail.com>
2026-01-08 13:08:51 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,10 +1,12 @@
 const coletanea = {
     "COLETANEA": [
-        { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus (Isaias Saad) - (Te amo Deus, tua graça nunca falha)" }
+        { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus - Isaias Saad - (Te amo Deus, tua graça nunca falha)" },
+        { "num": "Outro Igual Não Há (Ao Rei Nós Coroamos) - Gabriel Guedes", "titulo": "Outro Igual Não Há (Ao Rei Nós Coroamos)" }
         // ... adicione apenas a linha com número e título aqui
     ]
 
 };
+
 
 
 
