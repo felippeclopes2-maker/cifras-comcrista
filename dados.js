8d466477f5983c238126e306cfd4ecb54c63323b
felippeclopes2-maker <felippeclopes2@gmail.com>
2026-01-14 14:37:05 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,10 +3,11 @@ const coletanea = {
         { "num": "Bondade de Deus - Isaias Saad", "titulo": "Bondade de Deus - Isaias Saad (Te amo Deus, tua graça nunca falha)" },
         { "num": "Outro Igual Não Há (Ao Rei Nós Coroamos) - Gabriel Guedes", "titulo": "Outro Igual Não Há - Gabriel Guedes (Sobre o trono está, Jesus, Santo)" },
         { "num": "Um Novo Dia - Get Worship", "titulo": "Um Novo Dia - Get Worship" },
-        { "num": "Me Ama - Diante do Trono", "titulo": "Me Ama" },
+        { "num": "Me Ama - Diante do Trono", "titulo": "Me Ama - Diante do Trono (Tem ciumes de mim, o seu amor)" },
         { "num": "Dono da minha afeicao - FHOP", "titulo": "Dono da minha afeição - FHOP (Mais vale um dia em tua casa)" },
         { "num": "Santo Pra Sempre - Gabriel Guedes", "titulo": "Santo Pra Sempre - Gabriel Guedes (As muitas gerações rendidas em louvor)" }
     ]
 };
+
 
 
