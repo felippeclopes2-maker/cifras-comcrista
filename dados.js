74aca54779dd924fb81f9206551a483574a889cc
felippeclopes2-maker <felippeclopes2@gmail.com>
2026-01-15 12:34:05 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -5,9 +5,11 @@ const coletanea = {
         { "num": "Um Novo Dia - Get Worship", "titulo": "Um Novo Dia - Get Worship (Pela graça estou sendo curado)" },
         { "num": "Me Ama - Diante do Trono", "titulo": "Me Ama - Diante do Trono (Tem ciumes de mim, o seu amor)" },
         { "num": "Dono da minha afeicao - FHOP", "titulo": "Dono da minha afeição - FHOP (Mais vale um dia em tua casa)" },
-        { "num": "Santo Pra Sempre - Gabriel Guedes", "titulo": "Santo Pra Sempre - Gabriel Guedes (As muitas gerações rendidas em louvor)" }
+        { "num": "Santo Pra Sempre - Gabriel Guedes", "titulo": "Santo Pra Sempre - Gabriel Guedes (As muitas gerações rendidas em louvor)" },
+        { "num": "Cantarei - Comunidade", "titulo": "Cantarei - Comunidade (Tu és o próprio amor, terra e césu te adoram)" }
     ]
 };
+
 
 
 
